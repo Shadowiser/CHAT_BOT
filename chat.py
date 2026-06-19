33e52b095e7971a339d551861979bed3e14e966b
Shadowiser <tankomamoudou24@gmail.com>
2026-06-19 02:18:10 +0000

    bot basique fini

---
+++ chat.py
@@ -0,0 +1,60 @@
+import customtkinter as ctk
+
+#fonction de réponse du chatbot
+
+def chatbot_response(user_input):
+    responses = {
+        "bonjour" : "Bonjour comment puis-je aider ?",
+        "cv" : "Oui cv bien"
+    }
+    return responses.get(user_input.lower(), "Désolé je ne comprends pas la question")
+
+#fonction envoyer le message
+def send_message(event=None):
+    user_message = user_input.get()
+    if user_message.strip()!= "":
+        chat_history.configure(state="normal")
+        chat_history.insert("end",f"Vous: {user_message}\n", "user")
+        bot_response = chatbot_response(user_message)
+        chat_history.insert("end",f"Bot: {bot_response}\n", "bot")
+        chat_history.configure(state="disabled")
+        chat_history.see("end")
+        user_input.delete(0,"end")
+
+
+
+
+#config de l'interface
+app = ctk.CTk()
+app.geometry("500x600")
+app.title("Mon chatbot")
+
+#en tête
+
+header = ctk.CTkLabel(app,text="Bienvenue sur mon chatbot",font=("Nulshock",20,"bold"))
+header.pack(pady=10)
+
+#zone affichage du message
+
+chat_history = ctk.CTkTextbox(app, height=400,state="disabled")
+chat_history.tag_config("user", foreground="white")
+chat_history.tag_config("bot", foreground="green")
+chat_history.pack(pady=10,padx=10,fill="both",expand= True)
+chat_history.configure(font=("Orbitron",18,"bold"))
+
+
+# champ de saisie
+user_input_frame = ctk.CTkFrame(app)
+user_input_frame.pack(pady=10,padx=10,fill="x")
+
+user_input = ctk.CTkEntry(user_input_frame,placeholder_text="Entrez votre message...",width=350)
+user_input.pack(side="left",padx=5)
+
+send_btn = ctk.CTkButton(user_input_frame,text="Envoyer",command=send_message)
+
+send_btn.pack(side="left")
+
+#associer une touche pour envoyer le message
+
+app.bind("<Return>",send_message)
+app.mainloop()
